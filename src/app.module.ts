1e74d7366aaa31df3508a960a946a6f804203113
sai-oet <saiteja.malladi@oet.com.au>
2026-02-05 09:52:03 +1100

    refactor: move HTML content to separate file
    - Create public/index.html for test page
    - Use ServeStaticModule to serve static files
    - Update AppController to read HTML file from disk
    - Install @nestjs/serve-static dependency
    - Maintain same functionality with better separation of concerns

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { AppController } from './app.controller';
@@ -7,6 +9,9 @@ import { AppController } from './app.controller';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     ProductsModule,
     OrdersModule,
   ],
